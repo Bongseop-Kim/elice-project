17931be06211a4d7427a66acad65c5ffda77ec6c
bongsub kim <bong989971@gmail.com>
2023-06-16 14:32:08 +0900

    fix user json

--- job/getUser.ts
+++ job/getUser.ts
@@ -362,7 +362,7 @@ const pushData = async () => {
       phoneNumber: '010-0000-0000',
       role: Role.manager,
       hospitalId: hospital[i].id,
-      adminVerfied: 1,
+      adminVerfied: true,
     });
   }
 
